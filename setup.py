78d4ca1afd9e89802e7fa917a3a4d7301712d057
Benjamin Nigjeh <benjamin.nigjeh@gmail.com>
2024-04-30 17:03:36 -0700

    changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ms2",
+    version="0.0.1",
+    author="Ben Nigjeh",
+    author_email="benjamin.nigjeh@gmail.com",
+    install_requires=["pytheomics"],
+    packages=find_packages()
+)
