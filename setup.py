a4bc2dad3bcc4e51711941c05457a4f8e5059de6
Benjamin Nigjeh <benjamin.nigjeh@gmail.com>
2024-05-08 11:05:18 -0700

    changes

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name="ms2",
-    version="0.0.7",
+    version="0.0.8",
     author="Ben Nigjeh",
     author_email="benjamin.nigjeh@gmail.com",
-    install_requires=["pyteomics", "matplotlib", "pandas", "numpy", "wget", "h5py", "tensorflow", "keras", "spectrum_utils", "streamlit"],
+    install_requires=["pyteomics", "matplotlib", "pandas", "numpy", "wget", "h5py", "tensorflow", "keras", "spectrum_utils", "streamlit", "torch"],
     packages=find_packages()
 )
 
