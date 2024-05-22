89fb3ca996bb96b09cc8b1354c8b9b5200af4833
Benjamin Nigjeh <benjamin.nigjeh@gmail.com>
2024-05-21 21:45:37 -0700

    changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 from src.plot import plot
 
 # Page title
-st.title('API access to Generative RNN:streamlir r')
+st.title('API access to Generative RNN:')
 
 # Text input for the image name
 default_value = "MKWVTFISLLFLFSSAYSR"
